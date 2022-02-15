b1dcb4e5d2a3034dd62993dff73bf05a654af5d4
andres9508 <64918943+andres9508@users.noreply.github.com>
2022-02-14 20:25:49 -0500

    estrutura base

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+//Settings
+
+//Midlewares
+
+//Routes
+
+//Statin files
+
+app.listen(3000, ()=>{
+    console.log("server on port 3000")
+});
