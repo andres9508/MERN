008110459f8d1dc0399c0834e0e07344feb9fb6f
andres9508 <64918943+andres9508@users.noreply.github.com>
2022-02-14 21:56:10 -0500

    configurando back y conectado a mongodb online

---
+++ src/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const database = "Tasks";
+const password = "admin";
+const URI = 'mongodb+srv://admin:' + password + '@cluster0.aqkuo.mongodb.net/' + database + '?retryWrites=true&w=majority';
+
+mongoose.connect(URI)
+    .then(db => console.log("Atlas en linea"))
+    .catch(err => console.err(err))
+
+module.exports = mongoose;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+const{ mongoose } = require('./database');
+
 const app = express();
 
 //Settings
+app.set('port', process.env.PORT || 3000);
 
 //Midlewares
+//nos da imformacion respecto a las peticiones que hagamos
+app.use(morgan('dev'));
+//esta linea comprueba si el dato es un formato json
+app.use(express.json());
 
 //Routes
+app.use('/api/tasks',require('./routes/task.routes'));
 
 //Statin files
+//console.log(path.join(__dirname + '/public'));
+app.use(express.static(path.join(__dirname + '/public')));
 
-app.listen(3000, ()=>{
-    console.log("server on port 3000")
+//Starting the server
+app.listen(app.get('port'), ()=>{
+    console.log(`server on port ${app.get('port')}` )
 });
